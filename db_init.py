84c495905782a7f8517785b3c7e2e53de4a5fbdf
m-nikhil <m_nikhil@outlook.com>
2022-04-27 02:00:05 -0700

    fix typo

--- db_init.py
+++ db_init.py
@@ -2,6 +2,6 @@ from dotenv import load_dotenv
 from app.models import Base
 from app.database import connection
 
-load_dotenv('.env.stage')
+load_dotenv()
 connection.init()
 Base.metadata.create_all(bind=connection.engine)
